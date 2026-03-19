5d96bb68b9afbb83ef6f2dad303a65b91dc5b6d2
xsslush <reis.ana0812@gmail.com>
2026-03-19 14:47:35 -0300

    project done

--- organizer.py
+++ organizer.py
@@ -6,34 +6,45 @@ files = os.listdir('/home/lush/Downloads/Test')
 for file in files:
     print(file)
 
-# check file extensions
-for file in files:
-    extension = os.path.splitext(file)[-1].lower()
-    if extension == '.txt':
-        print(file, 'is txt')
-    elif extension == '.png':
-        print(file, 'is png')
-    elif extension == '.pdf':
-        print(file, 'is pdf')
-    else:
-        print(file, 'unknown file format')
+txt_directory = '/home/lush/Downloads/Test/TXT'
+media_directory = '/home/lush/Downloads/Test/Media'
+pdf_directory = '/home/lush/Downloads/Test/PDFs'
 
 # create new folders
-txt_directory = '/home/lush/Downloads/Test/TXT'
-img_directory = '/home/lush/Downloads/Test/Images'
-pdf_directory = '/home/lush/Downloads/Test/PDFs'
 if not os.path.exists(txt_directory):
     os.mkdir(txt_directory)
     print(f'{txt_directory} directory created')
-if not os.path.exists(img_directory):
-    os.mkdir(img_directory)
-    print(f'{img_directory} directory created')
+if not os.path.exists(media_directory):
+    os.mkdir(media_directory)
+    print(f'{media_directory} directory created')
 if not os.path.exists(pdf_directory):
     os.mkdir(pdf_directory)
     print(f'{pdf_directory} directory created')
 
-# moving files
+# check file extensions + moving files
+files_test = '/home/lush/Downloads/Test'
 for file in files:
-    move = extension
-    if move :
-        shutil.move(file, )
+    extension = os.path.splitext(file)[-1].lower()
+    if extension.endswith('.txt'):
+        source_path = os.path.join(files_test, file)
+        destination = os.path.join(txt_directory, file)
+        if os.path.exists(destination):
+            print('file already exists')
+        shutil.move(source_path, txt_directory)
+        print(file, 'moved to its directory')
+    elif extension.endswith('.png'):
+        source_path = os.path.join(files_test, file)
+        destination = os.path.join(media_directory, file)
+        if os.path.exists(destination):
+            print('file already exists')
+        shutil.move(source_path, media_directory)
+        print(file, 'moved to its directory')
+    elif extension.endswith('.pdf'):
+        source_path = os.path.join(files_test, file)
+        destination = os.path.join(pdf_directory, file)
+        if os.path.exists(destination):
+            print('file already exists')
+        shutil.move(source_path, pdf_directory)
+        print(file, 'moved to its directory')
+    else:
+        print(file, 'unknown file format')
